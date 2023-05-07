c955b479ecf42b98e881bb915abbd22b40a296e3
al-imam <alimam01828@gmail.com>
2023-05-07 15:09:16 +0600

    extends StoreType as object and use AtLeastOne type

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import { useReducer } from "react";
 type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> &
   U[keyof U];
 
-function useObjectStore<StoreType>(init: StoreType) {
+function useObjectStore<StoreType extends object>(
+  init: StoreType
+): [StoreType, React.Dispatch<AtLeastOne<StoreType>>] {
   return useReducer(
-    (prev: StoreType, next: Partial<StoreType>): StoreType => ({
+    (prev: StoreType, next: AtLeastOne<StoreType>): StoreType => ({
       ...prev,
       ...next,
     }),
