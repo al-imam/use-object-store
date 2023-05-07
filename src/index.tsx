3824ad2b2c5cbf7d0dd7d3d948e09d4f6585defb
al-imam <alimam01828@gmail.com>
2023-05-07 15:06:52 +0600

    copy atLeastOne type from stackoverflow

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import { useReducer } from "react";
 
+type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> &
+  U[keyof U];
+
 function useObjectStore<StoreType>(init: StoreType) {
   return useReducer(
     (prev: StoreType, next: Partial<StoreType>): StoreType => ({
